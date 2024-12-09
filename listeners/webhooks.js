9f0c74616a97e183a6a1c59a836911811c0828d4
germanm98 <germanmanzur98@gmail.com>
2024-12-09 00:08:01 -0300

    add listeners to package.json

--- listeners/webhooks.js
+++ listeners/webhooks.js
@@ -15,6 +15,27 @@ listeners.defaultSlackEvents = {
     callback: function(event) {
         let body = event.data.body;
         if (pkg.slack.utils.verifySignature(body.token)) {
+            return body;
+            sys.events.triggerEvent("slack:event", event.data.body);
+        } else {
+            sys.logs.warn('[slack] Invalid verification token for event');
+        }
+    }
+};
+
+listeners.slackChallenge = {
+    label: 'Catch Slack challenge',
+    type: 'service',
+    options: {
+        service: 'http',
+        event: 'webhookSync',
+        matching: {
+            path: '/slack-events/challenge',
+        }
+    },
+    callback: function(event) {
+        let body = event.data.body;
+        if (pkg.slack.utils.verifySignature(body.token)) {
             if (body.type === 'url_verification') {
                 return {challenge: body.challenge};
             }
