25c74780d384f06cff8704a4b6f97820241f657f
germanm98 <germanmanzur98@gmail.com>
2024-12-10 01:27:56 -0300

    removed unnecessary listener

--- listeners/webhooks.js
+++ listeners/webhooks.js
@@ -14,30 +14,11 @@ listeners.defaultSlackEvents = {
     },
     callback: function(event) {
         let body = event.data.body;
-        if (pkg.slack.utils.verifySignature(body.token)) {
-            return body;
-            sys.events.triggerEvent("slack:event", event.data.body);
-        } else {
-            sys.logs.warn('[slack] Invalid verification token for event');
-        }
-    }
-};
-
-listeners.slackChallenge = {
-    label: 'Catch Slack challenge',
-    type: 'service',
-    options: {
-        service: 'http',
-        event: 'webhookSync',
-        matching: {
-            path: '/challenge',
-        }
-    },
-    callback: function(event) {
-        let body = event.data.body;
         if (pkg.slack.utils.verifyToken(body.token)) {
             if (body.type === 'url_verification') {
                 return JSON.stringify({challenge: body.challenge});
+            } else {
+                sys.events.triggerEvent("slack:event", event.data.body);
             }
         } else {
             sys.logs.warn('[slack] Invalid verification token for event');
