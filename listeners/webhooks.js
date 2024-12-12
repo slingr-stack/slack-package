6da462360d12de4440e1552f41f7378e0e7fda2c
germanm98 <germanmanzur98@gmail.com>
2024-12-12 16:09:24 -0300

    fixed triggered event

--- listeners/webhooks.js
+++ listeners/webhooks.js
@@ -19,33 +19,7 @@ listeners.defaultSlackEvents = {
                 return JSON.stringify({challenge: body.challenge});
             } else {
                 sys.logs.info('Valid sync webhook received. Triggering event.');
-                sys.events.triggerEvent("slack:webhook", event.data);
-            }
-        } else {
-            sys.logs.warn('[slack] Invalid verification token for event');
-        }
-    }
-};
-
-listeners.defaultSlackWebhook = {
-    label: 'Testing webhook',
-    type: 'service',
-    options: {
-        service: 'http',
-        event: 'webhook',
-        matching: {
-            path: '/slack-events',
-        }
-    },
-    callback: function(event) {
-        let body = event.data.body;
-        if (pkg.slack.utils.verifyToken(body.token)) {
-            if (body.type === 'url_verification') {
-                return JSON.stringify({challenge: body.challenge});
-            } else {
-                sys.logs.info('Valid webhook received. Triggering event.');
-                sys.events.triggerEvent("slack:webhook", event.data);
-                return "ok";
+                sys.events.triggerEvent("slack:httpEventArrived", event.data);
             }
         } else {
             sys.logs.warn('[slack] Invalid verification token for event');
