aa84a66ce863ad28d69e200d7692280815e87618
germanm98 <germanmanzur98@gmail.com>
2024-12-13 00:37:14 -0300

    Added missing listeners

--- listeners/webhooks.js
+++ listeners/webhooks.js
@@ -9,18 +9,82 @@ listeners.defaultSlackEvents = {
         service: 'http',
         event: 'webhookSync',
         matching: {
-            path: '/slack-events',
+            path: '/slack/events',
         }
     },
     callback: function(event) {
-        let body = event.data.body;
-        if (pkg.slack.utils.verifyToken(body.token)) {
+        sys.logs.info('Received Slack webhook. Processing and triggering a package event.');
+        if (pkg.slack.utils.verifyToken(event.data.body.token)) {
             if (body.type === 'url_verification') {
-                return JSON.stringify({challenge: body.challenge});
+                sys.logs.info('Valid url verification. Sending challenge back.');
+                return JSON.stringify({challenge: event.data.body.challenge});
             } else {
-                sys.logs.info('Valid sync webhook received. Triggering event.');
-                sys.events.triggerEvent("slack:httpEventArrived", event.data);
+                sys.logs.info('Valid slack event received. Triggering event.');
+                sys.events.triggerEvent("slack:event", event.data);
             }
+        } else {
+            sys.logs.warn('[slack] Invalid verification token for event');
+        }
+    }
+};
+
+listeners.defaultSlashCommands = {
+    label: 'Catch Slash Commands',
+    type: 'service',
+    options: {
+        service: 'http',
+        event: 'webhook',
+        matching: {
+            path: '/slack/slashCommands',
+        }
+    },
+    callback: function(event) {
+        sys.logs.info('Received Slack webhook. Processing and triggering a package event.');
+        if (pkg.slack.utils.verifyToken(event.data.body.token)) {
+                sys.logs.info('Valid slash command received. Triggering package event.');
+                sys.events.triggerEvent("slack:slashCommand", event.data);
+        } else {
+            sys.logs.warn('[slack] Invalid verification token for event');
+        }
+    }
+};
+
+listeners.defaultInteractiveMessages = {
+    label: 'Catch Interactive Messages',
+    type: 'service',
+    options: {
+        service: 'http',
+        event: 'webhook',
+        matching: {
+            path: '/slack/interactiveMessages',
+        }
+    },
+    callback: function(event) {
+        sys.logs.info('Received Slack webhook. Processing and triggering a package event.');
+        if (pkg.slack.utils.verifyToken(event.data.body.token)) {
+            sys.logs.info('Valid interactive message received. Triggering package event.');
+            sys.events.triggerEvent("slack:interactiveMessages", event.data);
+        } else {
+            sys.logs.warn('[slack] Invalid verification token for event');
+        }
+    }
+};
+
+listeners.defaultOptonLoads = {
+    label: 'Catch Option Loads',
+    type: 'service',
+    options: {
+        service: 'http',
+        event: 'webhook',
+        matching: {
+            path: '/slack/optionsLoad',
+        }
+    },
+    callback: function(event) {
+        sys.logs.info('Received Slack webhook. Processing and triggering a package event.');
+        if (pkg.slack.utils.verifyToken(event.data.body.token)) {
+            sys.logs.info('Valid option load received. Triggering package event.');
+            sys.events.triggerEvent("slack:optionsLoad", event.data);
         } else {
             sys.logs.warn('[slack] Invalid verification token for event');
         }
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,35 +1,4 @@
 /**
- * Verifies the signature of the given body using the provided signature coded in sha1 or sha256.
- *
- * @param {string} body                 - The body to be verified.
- * @param {string} signature            - The signature to be checked.
- * @param {string} signature256         - The signature256 to be checked.
- * @return {boolean}                    - True if the signature is valid, false otherwise.
- */
-exports.verifySignature = function (body, signature, signature256) {
-    sys.logs.info("Checking signature");
-    let verified = true;
-    let verified256 = true;
-    let secret = config.get("webhookSecret");
-    if (!body || body === "") {
-        sys.logs.warn("The body is null or empty");
-        return false;
-    }
-    if (!secret || secret === "" || !signature || signature === "" ||
-        !sys.utils.crypto.verifySignatureWithHmac(body, signature.replace("sha1=",""), secret, "HmacSHA1")) {
-        sys.logs.warn("Invalid signature sha1");
-        verified = false;
-    }
-    if (!secret || secret === "" ||  !signature256 ||!signature256 ||
-        !sys.utils.crypto.verifySignatureWithHmac(body, signature.replace("sha256=",""), secret, "HmacSHA256")) {
-        sys.logs.warn("Invalid signature sha 256");
-        verified256 = false;
-    }
-
-    return (verified || verified256);
-};
-
-/**
  * Verifies the token is the same as the verification token.
  *
  * @param {string} token                - The token to verify
