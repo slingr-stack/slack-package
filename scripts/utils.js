d07ded4d3648835383b0a04d10d581cf12ddfe9c
pasaperez <aperez@slingr.io>
2024-12-30 00:26:52 -0300

    Added util

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,20 @@
+/****************************************************
+ Public API - Generic Functions
+ ****************************************************/
+
+/**
+ * Gets a configuration from the properties.
+ *
+ * @param {string} property - The name of the property to get. If it is empty, return the entire configuration object.
+ * @return {string} - The value of the property or the whole object as string.
+ */
+exports.getConfiguration = function (property) {
+    if (!property) {
+        return config.get();
+    }
+    return config.get(property);
+};
+
 /**
  * Verifies the token is the same as the verification token.
  *
@@ -5,5 +22,5 @@
  * @return {boolean}                    - True if the signature is valid, false otherwise.
  */
 exports.verifyToken = function (token) {
-    return token == config.get("verificationToken");
+    return token === config.get("verificationToken");
 };
